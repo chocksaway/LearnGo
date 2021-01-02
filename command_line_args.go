f01318d24b4d52d4d9b0e6f66ecb9e8d1a7f60d0
milesd <milesd@chocksaway.com>
2021-01-02 15:46:48 +0000

    initial commmit - parse command line

---
+++ command_line_args.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+/*
+	Check for exactly two arguments
+*/
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Please give one or more arguments")
+		os.Exit(1)
+	} else if len(os.Args) != 3 {
+		fmt.Println("Two arguments expected")
+		os.Exit(1)
+	}
+
+	arguments := os.Args
+	min, _ := strconv.ParseFloat(arguments[1], 64)
+	max, _ := strconv.ParseFloat(arguments[1], 64)
+
+	for i := 2; i < len(arguments); i++ {
+		n, _ := strconv.ParseFloat(arguments[i], 64)
+		if n < min {
+			min = n
+		}
+		if n > max {
+			max = n
+		}
+	}
+
+	fmt.Println("Min:", min)
+	fmt.Println("Max:", max)
+}
