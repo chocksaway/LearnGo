058d947e5ae89115b6cc719edef189bfa2d286e6
milesd <milesd@chocksaway.com>
2021-01-14 06:00:16 +0000

    Multidimensional arrays and range.

--- arrays.go
+++ arrays.go
@@ -14,4 +14,25 @@ func main() {
 	}
 
 	fmt.Println("Exited loop")
+
+	var twoD = [4][4]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12},
+		{13, 14, 15, 16}}
+
+	fmt.Println(len(twoD[0]))
+
+	var counterInner int
+	for counter = 0; counter < len(twoD); counter++ {
+		for counterInner = 0; counterInner < len(twoD[counter]); counterInner++ {
+			fmt.Print(twoD[counter][counterInner])
+		}
+	}
+
+	fmt.Println("\nusing _")
+
+	for _, outer := range twoD {
+		for _, inner := range outer {
+			fmt.Print(inner)
+		}
+	}
+
 }
