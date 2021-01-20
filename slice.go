d50b13f1cca934310633649aa4891bcb0a55fccf
milesd <milesd@chocksaway.com>
2021-01-20 12:43:22 +0000

    Slice copy.

--- slice.go
+++ slice.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	var intSlice = []int{1, 2, 3, 4, 5}
+	var intSlice2 = []int{-1, 6}
+
+	// destination source
+	copy(intSlice, intSlice2)
 
 	fmt.Println(intSlice)
+
+	fmt.Println(intSlice2)
 
 	fmt.Printf("Cap: %d, Length: %d\n", cap(intSlice), len(intSlice))
 
