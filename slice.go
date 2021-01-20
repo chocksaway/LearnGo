d7493ada82f551abee055276cb741e5ef028c32c
milesd <milesd@chocksaway.com>
2021-01-20 12:31:30 +0000

    Slice length and capacity.

--- slice.go
+++ slice.go
@@ -5,13 +5,17 @@ import "fmt"
 func main() {
 	var intSlice = []int{1, 2, 3, 4, 5}
 
-	fmt.Print(intSlice)
+	fmt.Println(intSlice)
+
+	fmt.Printf("Cap: %d, Length: %d\n", cap(intSlice), len(intSlice))
 
 	intSlice = append(intSlice, 6)
 
 	partSlice := intSlice[1:3]
 
-	fmt.Print(intSlice)
+	fmt.Println(intSlice)
+
+	fmt.Printf("Cap: %d, Length: %d\n", cap(intSlice), len(intSlice))
 
 	fmt.Print(intSlice[1:3])
 
